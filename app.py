7c3d6f1bdb11f494ab3fffdad9b9ed41dea4db57
Lukas Froehlich <lukasfroehlich1@gmail.com>
2015-09-03 20:53:22 -0700

    trying to repuhs

--- app.py
+++ app.py
@@ -2,16 +2,7 @@ from flask import Flask
 from scheduler import *
 
 app = Flask(__name__)
+print "things are getting started"
 
 if __name__ == '__main__':
     app.run()
-
-#ksched.add_daytime_task(action=send_question,
-#k                       taskname="send_quesion",
-#k                       weekdays=[1,2,3,4,5,6,7],
-#k                       monthdays=None,
-#k                       timeonday=[1,22],
-#k                       processmethod=kronos.method.threaded,
-#k                       args=None,
-#k                       kw=None)
-
