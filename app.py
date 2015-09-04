6dabcdfa5e93a11cc032d3cc718e9c31eeaa4bcb
Lukas Froehlich <lukasfroehlich1@gmail.com>
2015-09-03 21:24:00 -0700

    stupid mistake

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask
 #from scheduler import *
+from test_scheduler import *
 
 app = Flask(__name__)
 print "things are getting started"
 
 @app.route('/')
 def hello_world():
-    from test_scheduler import *
     return 'hello world!'
 
 if __name__ == '__main__':
