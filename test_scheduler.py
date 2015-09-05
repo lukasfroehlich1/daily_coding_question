8f16b260d93019de5e344b55d2094bd9d6873efd
Lukas Froehlich <lukasfroehlich1@gmail.com>
2015-09-04 22:55:36 -0700

    testing, I should get one email from programmingquestions

--- test_scheduler.py
+++ test_scheduler.py
@@ -1,20 +1,33 @@
 import kronos
+import time
+import smtplib
+import os
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+from email.mime.multipart import MIMEMultipart
 
+class qScheduler():
 
-class test_scheduler():
+    def send_test_email():
+        fromaddr = 'programmingquestions1@gmail.com'
+        toaddrs = 'lukasfroehlich1@gmail.com'
+        server = smtplib.SMTP("smtp.gmail.com:587")
+        server.starttls()
+        server.login("programmingquestions1","Coding123")
 
-    def test_output():
-        print "TESTING!!!!"
+        server.sendmail(fromaddr, toaddrs, "yoooooo")
+        server.quit()
+
 
     sched = kronos.ThreadedScheduler()
     sched.start()
+    print "scheduler created"
 
-    sched.add_interval_task(action=test_output,
-                            taskname="testoutput",
-                            initialdelay=1,
-                            interval=2,
-                            processmethod=kronos.method.threaded,
-                            args=None,
-                            kw=None)
+    sched.add_single_task(action=send_test_email,
+                          taskname="testoutput",
+                          initialdelay=1,
+                          processmethod=kronos.method.threaded,
+                          args=None,
+                          kw=None)
 
-sched = test_scheduler()
+sched = qScheduler()
