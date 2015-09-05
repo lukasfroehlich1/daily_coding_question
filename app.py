515b76d9b6fbe5bfad3b12f403034f72bb39825e
Lukas Froehlich <lukasfroehlich1@gmail.com>
2015-09-05 15:15:58 -0700

    setup the service to send at 10 in the morning everyday

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 from flask import Flask
-#from scheduler import *
-from test_scheduler import *
+from scheduler import *
+#from test_scheduler import *
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/')
 print "things are getting started"
 
 @app.route('/')
 def hello_world():
-    return 'hello world!'
+    return app.send_static_file('index.html')
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, debug=True)
